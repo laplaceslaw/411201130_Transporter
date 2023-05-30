7f27f9254d97df8654dea94f204fc95559daa9c5
indra <411201130@mahasiswa.undira.ac.id>
2023-05-30 22:17:44 +0700

    Upload ke Git

---
+++ config/jwt.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | JWT Secret
+    |--------------------------------------------------------------------------
+    |
+    | This value is used to sign your tokens. Make sure to keep it secret!
+    |
+    */
+
+    'secret' => '1234567',
+
+    // ... konfigurasi lainnya ...
+
+];
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Indra Surya Hanjaya',
+            'email' => 'Indra321@gmail.com',
+            'password' => Hash::make('1234567),
+        ]);
+
+        // Tambahkan data pengguna lainnya di sini jika diperlukan
+
+    }
+}
